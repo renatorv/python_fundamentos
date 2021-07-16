552bdbcf6bcf4855b46ba65f6c1d7da3dc67fb3f
renato alves <renatorv@gmail.com>
2021-07-16 16:53:59 -0300

    teste remove folder

---
+++ main.py
@@ -0,0 +1,23 @@
+
+# nome = 'Renato'
+# idade = 25
+# sobrenome = 'Alves'
+
+nome, idade, sobrenome = 'Renato', 33, 'Alves'
+
+# print(nome, sobrenome, idade)
+
+# print('Olá', nome, sobrenome, 'sua idade é', idade)
+
+# print(f'Olá, {nome} {sobrenome}, sua idade é {idade} ')
+
+
+nome1 = input('Digite o seu nome: ')
+
+sobrenome1 = input('Digite o seu sobrenome: ')
+
+idade1 = int(input('Digite sua idade: '))
+
+print(f'Olá, {nome1} {sobrenome1}, sua idade é {idade1} ')
+
+print(type(idade1))
